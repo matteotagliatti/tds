a95f8411a7faa26ceeedca55869770fdec05e373
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-02-25 11:20:53 +0100

    list elements

--- commands/list.js
+++ commands/list.js
@@ -1,1 +1,17 @@
-export function list() {}
+import { currentWorkingDirectory, fs } from "../utils/shared.js";
+
+export function list() {
+  const fileData = fs
+    .readFileSync(currentWorkingDirectory + "todo.md")
+    .toString();
+  let data = fileData.split("\n");
+  data = data.filter((item) => item !== "");
+
+  if (data.length === 0) {
+    console.log("There are no pending todos!");
+  }
+
+  data.forEach((item, index) => {
+    console.log(`${index + 1}. ${item}`);
+  });
+}
